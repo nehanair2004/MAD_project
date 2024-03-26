90e2bdbb9d755d00ae97b11bdafe8ff00f38cc00
Neha Nair <neha134learner@gmail.com>
2024-03-27 01:05:43 +0530

    Finished exp5

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+
+import 'package:trial/firebase_options.dart';
+import 'package:trial/screens/login_exp.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:trial/firebase_options.dart';
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(home: Signin(),debugShowCheckedModeBanner: false,));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Firebase"),
+      ),
+    );
+  }
+}
